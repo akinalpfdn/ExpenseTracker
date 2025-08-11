3e4e0ec30da246face39c71927ec54f47d426f08
akinalpfdn <akinalp97@gmail.com>
2025-08-11 09:39:24 +0300

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Akinalp Fidan on 11.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
